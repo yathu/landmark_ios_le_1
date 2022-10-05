ad780271d206b0def8704c257e509fb6dc4c9667
yathavanyogarajah <yathavan@surge.global>
2022-10-05 13:12:29 +0530

    Initial Commit

---
+++ landmarks/landmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarksApp.swift
+//  landmarks
+//
+//  Created by Yathavan Yogarajah on 2022-10-05.
+//
+
+import SwiftUI
+
+@main
+struct landmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
